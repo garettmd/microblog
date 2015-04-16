f37a0bf0e96303615e95e2359440f4e41bf3c8e5
Garett Dunn <garettmd@gmail.com>
2015-04-16 16:33:42 -0500

    Done with part 5

--- app/views.py
+++ app/views.py
@@ -4,10 +4,12 @@ from app import app, db, lm, oid
 from .forms import LoginForm
 from .models import User
 
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
+    user = g.user
     posts = [
         {
             'author': {'nickname': 'John'},
@@ -19,10 +21,10 @@ def index():
         }
     ]
     return render_template('index.html',
-                            #title='Home',
-                            title = 'Home',
-                            user = user,
-                            posts = posts)
+                           # title='Home',
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -35,9 +37,41 @@ def login():
         session['remember_me'] = form.remember_me.data
         return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html',
-                            title='Sign In',
-                            form=form,
-                            providers=app.config['OPENID_PROVIDERS'])
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 
 @lm.user_loader
